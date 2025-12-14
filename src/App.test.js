7916122f79d1aab70d70b168285012fda555fcbe
Urii <mandziy.yurko@gmail.com>
2025-12-14 22:56:59 +0200

    Fix

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Hello/i);
+  const linkElement = screen.getByText(/My dream team/i);
   expect(linkElement).toBeInTheDocument();
 });
